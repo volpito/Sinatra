0b4c7b0d19c5c630446c41263e28788d7eb2a4ae
volpito <mpj.rigot@gmail.com>
2021-02-01 09:54:30 +0100

    arbo2

--- hello_world_sinatra/app.rb
+++ hello_world_sinatra/app.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/hello' do
   '<h1>Hello world ! </h1>'
 end
+
+get '/bonjour' do
+  '<h1>Bonjour, monde !</h1>'
+end
+
+get '/tg_sale_con' do
+  '<p>"Y a pas de bonne ou de mauvaise situations. Je dirai que vie c est avanat toutes des <a href> https://www.facebook.com recontres \n" </a></p>'
+end
